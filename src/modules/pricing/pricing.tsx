24dd8caef6fc21a6b88497cf2eef24714255627a
SinaSfr <anonymous8884person@gmail.com>
2024-11-12 15:19:43 +0330

    home updated

--- src/modules/pricing/pricing.tsx
+++ src/modules/pricing/pricing.tsx
@@ -1,11 +1,13 @@
 // "use client";
 import myImage from "@/assets/images/user-img/user-img1.jpg";
+import { AddShoppingCart } from "@mui/icons-material";
 import CheckIcon from "@mui/icons-material/Check";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import {
   Box,
   Button,
   Grid,
+  Grid2,
   IconButton,
   Table,
   TableBody,
@@ -20,101 +22,178 @@ import Image from "next/image";
 const PricingTable = () => {
   return (
     <Box className="pt-24 pb-16">
-      <Box component="form">
-    <TableContainer>
-      <TableHead>
-        <TableRow>
-          <TableCell>آنچه شما دریافت می کنید</TableCell>
-          <TableCell className="">طرح توسعه</TableCell>
-          <TableCell className="border-t border-l">پست 22 شغل</TableCell>
-        </TableRow>
-      </TableHead>
+      <Grid2 container spacing={6} justifyContent="center" alignItems="center">
+        <Grid2 size={6} display="flex" justifyContent="end">
+          <Box component="form">
+            <TableContainer>
+              <TableHead>
+                <TableRow>
+                  <TableCell className="border-none"></TableCell>
+                  <TableCell
+                    className="text-lightGreen border-none text-xl"
+                    align="center"
+                  >
+                    250 تومان
+                    <Typography className="text-mainGray">/ ماه</Typography>
+                  </TableCell>
+                  <TableCell
+                    className="text-lightGreen border-none text-xl"
+                    align="center"
+                  >
+                    500 تومان
+                    <Typography className="text-mainGray">/ ماه</Typography>
+                  </TableCell>
+                </TableRow>
 
-      <TableBody>
-        <TableRow>
-          <TableCell align="center">پست های شغلی شامل</TableCell>
-          <TableCell align="center" className="border-x">
-            پست 10 شغل
-          </TableCell>
-          <TableCell align="center" className="border-t border-l">
-            پست 22 شغل
-          </TableCell>
-        </TableRow>
+                <TableRow>
+                  <TableCell className="text-xl">آنچه شما دریافت می کنید</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-t border-r bg-lightGreenXXs text-lightGreen text-xl"
+                  >
+                    طرح توسعه
+                  </TableCell>
+                  <TableCell align="center" className="border-t border-x text-xl">
+                    طرح حق بیمه
+                  </TableCell>
+                </TableRow>
+              </TableHead>
 
-        <TableRow>
-          <TableCell align="center">شغل ویژه</TableCell>
-          <TableCell align="center" className="border-x">
-            20
-          </TableCell>
-          <TableCell align="center" className="border-t border-l">
-            نامحدود
-          </TableCell>
-        </TableRow>
+              <TableBody>
+                <TableRow>
+                  <TableCell align="right">پست های شغلی شامل</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    پست 10 شغل
+                  </TableCell>
+                  <TableCell align="center" className="border-l">
+                    پست 22 شغل
+                  </TableCell>
+                </TableRow>
 
-        <TableRow>
-          <TableCell align="center"> روزهای نمایش داده شده</TableCell>
-          <TableCell align="center" className="border-x">
-            به مدت 10 روز
-          </TableCell>
-          <TableCell align="center" className="border-t border-l">
-            به مدت 50 روز
-          </TableCell>
-        </TableRow>
+                <TableRow>
+                  <TableCell align="right">شغل ویژه</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    20
+                  </TableCell>
+                  <TableCell align="center" className="border-l">
+                    نامحدود
+                  </TableCell>
+                </TableRow>
 
-        <TableRow>
-          <TableCell align="center"> امار ارسال شغل</TableCell>
-          <TableCell align="center" className="border-x">
-            2
-          </TableCell>
-          <TableCell align="center" className="border-t border-l">
-            3
-          </TableCell>
-        </TableRow>
+                <TableRow>
+                  <TableCell align="right"> روزهای نمایش داده شده</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    به مدت 10 روز
+                  </TableCell>
+                  <TableCell align="center" className="border-l">
+                    به مدت 50 روز
+                  </TableCell>
+                </TableRow>
 
-             <TableRow>
-        <TableCell align="center"> پست های هایلایت شده</TableCell>
-        <TableCell align="center" className="border-x">
-          5 پست
-        </TableCell>
-        <TableCell align="center" className="border-t border-l">
-          نامحدود
-        </TableCell>
-      </TableRow>
+                <TableRow>
+                  <TableCell align="right"> امار ارسال شغل</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    <CheckIcon />
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-l text-lightGreen"
+                  >
+                    <CheckIcon />
+                  </TableCell>
+                </TableRow>
 
-      <TableRow>
-        <TableCell align="center"> روز های انقضای فهرست</TableCell>
-        <TableCell align="center" className="border-x">
-          60 روز
-        </TableCell>
-        <TableCell align="center" className="border-t border-l">
-        
-          200 روز
-        </TableCell>
-      </TableRow>
+                <TableRow>
+                  <TableCell align="right"> پست های هایلایت شده</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    5 پست
+                  </TableCell>
+                  <TableCell align="center" className="border-l">
+                    نامحدود
+                  </TableCell>
+                </TableRow>
 
-      <TableRow>
-        <TableCell align="center"> پشتیبانی ایمیل</TableCell>
-        <TableCell align="center" className="border-x">
-        
-          20 روز
-        </TableCell>
-        <TableCell align="center" className="border-t border-l">
-        
-          3
-        </TableCell>
-      </TableRow>
-      </TableBody>
-    </TableContainer>
-     </Box>
+                <TableRow>
+                  <TableCell align="right"> روز های انقضای فهرست</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    60 روز
+                  </TableCell>
+                  <TableCell align="center" className="border-l">
+                    200 روز
+                  </TableCell>
+                </TableRow>
 
-    /* Pricing Content */
-     <Grid item xs={12} md={6}>
+                <TableRow>
+                  <TableCell align="right"> پشتیبانی ایمیل</TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    20 روز
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-l text-lightGreen"
+                  >
+                    <CheckIcon />
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell align="center" className="border-none"></TableCell>
+                  <TableCell
+                    align="center"
+                    className="border-x bg-lightGreenXXs text-lightGreen"
+                  >
+                    <Button
+                      variant="contained"
+                      endIcon={<AddShoppingCart />}
+                      className="bg-lightGreen text-mainWhite gap-3 px-5 py-3"
+                    >
+                      خرید
+                    </Button>
+                  </TableCell>
+                  <TableCell align="center" className="border-l">
+                    <Button
+                      variant="contained"
+                      endIcon={<AddShoppingCart />}
+                      className="bg-lightGreen text-mainWhite gap-3 px-5 py-3"
+                    >
+                      خرید
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </TableContainer>
+          </Box>
+        </Grid2>
+        {/* Pricing Content  */}
+
+        <Grid2 size={6}>
           <Box className="pl-5">
             <Typography variant="h4" gutterBottom>
               بهترین روش های <b className="text-lightGreen">قیمت گذاری</b>
             </Typography>
             <Box className="mb-4 w-24 h-1 bg-gray-400" />
-            <Typography variant="body1" paragraph>
+            <Typography variant="body1" className="max-w-[38rem]" paragraph>
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
               استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
               در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
@@ -173,8 +252,9 @@ const PricingTable = () => {
               2000+ کارفرمای برتر
             </Typography>
           </Box>
-        </Grid> 
-    // </Box>
+        </Grid2>
+      </Grid2>
+    </Box>
   );
 };
 
