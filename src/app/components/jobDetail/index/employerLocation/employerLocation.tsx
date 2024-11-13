ef8e0c4ad597a36b781fcece6021807cf0f903f9
Rwiiin <rwiiinzakeri@gmail.com>
2024-11-13 08:23:31 +0330

    typo

--- src/app/components/jobDetail/index/employerLocation/employerLocation.tsx
+++ src/app/components/jobDetail/index/employerLocation/employerLocation.tsx
@@ -1,15 +1,15 @@
 import AnimateLine from "@/modules/animateLine/animateLine";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const EmployerLocation = () => {
-    return ( 
-        <Box className="  p-4 shadow-slate-400 boxShadowJob mt-[30px]">
-        <h2 className="text-[22px]">محل کارفرما  </h2>
-        <Box className="w-[30%]">
-          <AnimateLine />
-        </Box>
+  return (
+    <Box className="  p-4 shadow-slate-400 boxShadowJob mt-[30px]">
+      <Typography className="text-[22px]">محل کارفرما </Typography>
+      <Box className="w-[30%]">
+        <AnimateLine />
       </Box>
-     );
-}
- 
+    </Box>
+  );
+};
+
 export default EmployerLocation;
