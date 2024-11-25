f91d824cbae78055761c6ad1fd37127946f0336a
mohamadkzd <kazemzademohammad4@gmail.com>
2024-11-25 18:57:01 +0330

    contact

---
+++ src/app/components/contactUs/contact-area/contactArea.tsx
@@ -0,0 +1,112 @@
+"use client";
+import {
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormHelperText,
+  Typography,
+  Box,
+  Container,
+} from "@mui/material";
+import { useState } from "react";
+const ContactArea = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [termsAccepted, setTermsAccepted] = useState(false);
+  const [errors, setErrors] = useState<{ email: boolean }>({ email: false });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!email) {
+      setErrors({ email: true });
+      return;
+    }
+
+    console.log({ email, name, phoneNumber, subject, message, termsAccepted });
+
+    // Reset form
+    setEmail("");
+    setName("");
+    setPhoneNumber("");
+    setSubject("");
+    setMessage("");
+    setTermsAccepted(false);
+    setErrors({ email: false });
+  };
+
+  return (
+    <Container
+      className="mt-[30px] w-[60%] mx-auto"
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ mt: 2 }}
+    >
+      <Typography variant="h4" mb={2} textAlign="center">
+        نظرات بدهید
+      </Typography>
+      <TextField
+        fullWidth
+        label="ایمیل شما"
+        variant="outlined"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        error={errors.email}
+        helperText={errors.email ? "لطفا ایمیل خود را وارد کنید" : ""}
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        fullWidth
+        label="نام شما"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        fullWidth
+        label="شماره تلفن"
+        variant="outlined"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        fullWidth
+        label="موضوع شما"
+        variant="outlined"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        fullWidth
+        label="پیام شما"
+        variant="outlined"
+        multiline
+        rows={4}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        sx={{ mb: 2 }}
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={termsAccepted}
+            onChange={(e) => setTermsAccepted(e.target.checked)}
+          />
+        }
+        label="پذیرفتن قوانین و شرایط و حریم خصوصی."
+      />
+      <Button type="submit" variant="contained" color="primary">
+        ارسال پیام
+      </Button>
+    </Container>
+  );
+};
+
+export default ContactArea;
---
+++ src/app/components/contactUs/section-title/sectionContact.tsx
@@ -0,0 +1,40 @@
+import { Box } from "@mui/material";
+import EditLocationAltIcon from "@mui/icons-material/EditLocationAlt";
+const SectionContact = () => {
+  return (
+    <Box className="my-[50px]">
+      <Box className="flex justify-center items-center gap-x-5 flex-wrap">
+        <Box className="relative transition-all duration-[500ms] hover:translate-y-[-5px] hover:border-[#b1fae8] w-[23%] border-[1px] border-[#f2f2f2] py-[25px] pr-[93px] pl-[18px] mt-[15px] mb-[30px] rounded-[10px]">
+          <Box className="">
+            <EditLocationAltIcon sx={{fontSize:8}} className="absolute top-[-15px] right-0 rounded-[10px] text-[#fa5f1c] bg-[#fee7dd] leading-[80px]  text-center w-[58px] h-[80px]" />
+          </Box>
+          <h3 className="text-[20px]">آدرس</h3>
+          <p className="text-[gray]">ایران-تهران</p>
+        </Box>
+        <Box className="relative transition-all duration-[500ms] hover:translate-y-[-5px] hover:border-[#b1fae8] w-[23%] border-[1px] border-[#f2f2f2] py-[25px] pr-[93px] pl-[18px] mt-[15px] mb-[30px] rounded-[10px]">
+          <Box className="">
+            <EditLocationAltIcon className="absolute top-[-15px] right-0 rounded-[10px] text-[#fa5f1c] bg-[#fee7dd] leading-[80px] text-[25px] text-center w-[58px] h-[80px]" />
+          </Box>
+          <h3 className="text-[20px]">آدرس</h3>
+          <p className="text-[gray]">ایران-تهران</p>
+        </Box>
+        <Box className="relative transition-all duration-[500ms] hover:translate-y-[-5px] hover:border-[#b1fae8] w-[23%] border-[1px] border-[#f2f2f2] py-[25px] pr-[93px] pl-[18px] mt-[15px] mb-[30px] rounded-[10px]">
+          <Box className="">
+            <EditLocationAltIcon className="absolute top-[-15px] right-0 rounded-[10px] text-[#fa5f1c] bg-[#fee7dd] leading-[80px] text-[25px] text-center w-[58px] h-[80px]" />
+          </Box>
+          <h3 className="text-[20px]">آدرس</h3>
+          <p className="text-[gray]">ایران-تهران</p>
+        </Box>
+        <Box className="relative transition-all duration-[500ms] hover:translate-y-[-5px] hover:border-[#b1fae8] w-[23%] border-[1px] border-[#f2f2f2] py-[25px] pr-[93px] pl-[18px] mt-[15px] mb-[30px] rounded-[10px]">
+          <Box className="">
+            <EditLocationAltIcon className="absolute top-[-15px] right-0 rounded-[10px] text-[#fa5f1c] bg-[#fee7dd] leading-[80px] text-[25px] text-center w-[58px] h-[80px]" />
+          </Box>
+          <h3 className="text-[20px]">آدرس</h3>
+          <p className="text-[gray]">ایران-تهران</p>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default SectionContact;
