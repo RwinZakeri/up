b85de0b9a02b1a5e681cd797a4159f5226a1c5be
rwiiin <rwiiinzakeri@gmail.com>
2024-12-18 19:11:37 +0330

    [addFeature] add dashboard folder and file

---
+++ src/app/(dashboard)/admin/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const AdminDashboard = () => {
+  return (
+    <Box>
+      <Typography>hello world</Typography>
+    </Box>
+  );
+};
+
+export default AdminDashboard;
---
+++ src/app/(dashboard)/admin/layout.tsx
@@ -0,0 +1,12 @@
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+      </div>
+      {children}
+    </>
+  );
+};
+
+export default AdminLayout;
